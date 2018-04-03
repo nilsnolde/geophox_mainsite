a4793a1f65d1fb913e92521cb66e0d1288889b16
nilsnolde <nils@openrouteservice.org>
2018-04-03 08:58:34 +0200

    added contact.html

---
+++ static/js/map_contact.js
@@ -0,0 +1,5 @@
+var mymap = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
--- static/js/map_front.js
+++
@@ -1,6 +0,0 @@
-var layer = new L.StamenTileLayer('watercolor');
-var map = new L.map('map_front', {
-          center: new L.LatLng(52.48635, 13.43054),
-          zoom:   10
-});
-map.addLayer(layer);
