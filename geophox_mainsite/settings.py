92ab01e3fd0aee57f02cf47c3e63f15c9ba2899b
nilsnolde <nils@openrouteservice.org>
2018-05-21 13:24:56 +0200

    linted syntax

--- geophox_mainsite/settings.py
+++ geophox_mainsite/settings.py
@@ -16,20 +16,20 @@ from django.core.exceptions import ImproperlyConfigured
 
 """Import settings"""
 with open(os.environ.get('SITE_CONFIG')) as f:
- configs = json.loads(f.read())
- print(configs)
+    configs = json.loads(f.read())
+    print(configs)
 
- def get_env_var(setting, configs=configs):
-  try:
-      val = configs[setting]
-      if val == 'True':
-          val = True
-      elif val == 'False':
-          val = False
-      return val
-  except KeyError:
-      error_msg = "ImproperlyConfigured: Set {0} environment variable".format(setting)
-      raise ImproperlyConfigured(error_msg)
+def get_env_var(setting, configs=configs):
+    try:
+        val = configs[setting]
+        if val == 'True':
+            val = True
+        elif val == 'False':
+            val = False
+        return val
+    except KeyError:
+        error_msg = "ImproperlyConfigured: Set {0} environment variable".format(setting)
+        raise ImproperlyConfigured(error_msg)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
